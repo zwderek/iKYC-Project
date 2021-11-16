e9e9f9c0ac6a915bf7fac33b99c07486a627d1cd
zwderek <61866929+zwderek@users.noreply.github.com>
2021-11-16 18:25:24 +0800

    Add files via upload

--- main_GUI/main.py
+++ main_GUI/main.py
@@ -8,8 +8,12 @@ from profile_edit import Ui_profile_edit
 from account_Info import Ui_account_Info
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QDialog
+import functions
 import pics_ui_rc
+from collections import OrderedDict
 
+from functions import *
+from FrontHelper import *
 
 
 class loginWindow(QMainWindow, Ui_login):
@@ -40,10 +44,17 @@ class signin_PasswordWindow(QWidget, Ui_signin_Password):
         self.pushButton.clicked.connect(lambda : self.signin())
 
     def signin(self):
+        loginWindow.close()
+        signin_PasswordWindow.close()
+
         signin_username = self.lineEdit.text()
         signin_password = self.lineEdit_2.text()
-        if True: #判断是否可以login
-            customer_InfoWindow.customer_username = signin_username
+        tmp = demo.password_login(signin_username, signin_password)
+        #print(signin_username, signin_password, tmp)
+        if tmp > 0:
+            customer_InfoWindow.customer_id = tmp
+            customer_InfoWindow.initUi() #update
+            customer_InfoWindow.addButton()
             customer_InfoWindow.show()
 
 class signupWindow(QWidget, Ui_signup):
@@ -59,33 +70,74 @@ class signupWindow(QWidget, Ui_signup):
         self.pushButton.clicked.connect(lambda: self.signup())
 
     def signup(self):
+        loginWindow.close()
+        signin_PasswordWindow.close()
+
         signup_username = self.lineEdit.text()
         signup_password_1 = self.lineEdit_2.text()
         signup_password_2 = self.lineEdit_3.text()
-        if True:  # 判断是否可以login 并创建用户
-            customer_InfoWindow.customer_username = signup_username
-            customer_InfoWindow.show()
+
+        if signup_password_1 == signup_password_2:  # 判断是否可以login 并创建用户
+            tmp = demo.create_user(signup_username, signup_password_1)
+            print(tmp)
+            if tmp > 0:
+                customer_InfoWindow.customer_id = tmp
+                customer_InfoWindow.initUi()
+                customer_InfoWindow.addButton()
+                customer_InfoWindow.show()
+            if tmp == -2:
+                self.lineEdit.setText("This name is already been used!")
+        else:
+            self.lineEdit_2.setText("Please reenter your password!")
+
 
 class customer_InfoWindow(QWidget, Ui_customer_Info):
-    customer_username = ""
+    customer_id = -1
+    display_customer_info = ""
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.initUi()
-        username = None
+    #   print(self.customer_id)
 
     def initUi(self):
-
-        self.textBrowser.setText("Welcome message and profile")
-        self.textBrowser_2.setText("Login History")
+        self.textBrowser.setText("Welcome! You can edit your own profile now!")
+        result = demo.display_login_info(self.customer_id)
+        self.display_customer_info = result["customer_and_login"]
+        self.display_user_profile = result["profile"]
+        if Util.isNotNone(self.display_customer_info):
+            username = self.display_customer_info[0]
+            login_time = self.display_customer_info[1]
+            login_date = self.display_customer_info[2]
+            to_pack = OrderedDict()
+            to_pack["username"] = username
+            to_pack["login_time"] = login_time
+            to_pack["login_date"] = login_date
+            # self.textBrowser_2.setText("username = " + username + "\nlogin_time = " + login_time + "\nlogin_date = " + login_time)
+            self.textBrowser_2.setText(FrontHelper.dictionaryToInfostring(to_pack))
+        if Util.isNotNone(self.display_user_profile):
+            profile_name = self.display_user_profile[0]
+            gender = self.display_user_profile[1] #tbd
+            birthday = self.display_user_profile[2]
+            email = self.display_user_profile[3]
+            welcome_msg = self.display_user_profile[5]
+            is_public = self.display_user_profile[6]
+            to_pack = OrderedDict()
+            to_pack["Welcome"] = welcome_msg
+            to_pack["profile_name"] = profile_name
+            to_pack["gender"] = FrontHelper.genderToString(gender)
+            to_pack["birthday"] = birthday
+            to_pack["email"] = email
+            to_pack["Profile status"] = FrontHelper.ispublicToString(is_public)
+            self.textBrowser.setText(FrontHelper.dictionaryToInfostring(to_pack))
 
         self.pushButton.clicked.connect(lambda: self.profile_edit())
 
-        _translate = QtCore.QCoreApplication.translate
-
         self.btns = []
 
+
+    def addButton(self):
         for i in range(5):
             self.btns.append( QtWidgets.QPushButton(self.verticalLayoutWidget))
             self.btns[i].setText(str(i))
@@ -94,7 +146,7 @@ class customer_InfoWindow(QWidget, Ui_customer_Info):
             self.btns[i].clicked.connect(lambda: self.account_Info(self.sender().text()))
 
     def profile_edit(self):
-        profile_editWindow.profile_username =self.customer_username
+        profile_editWindow.profile_customer_id = self.customer_id
         profile_editWindow.show()
 
     def account_Info(self, id):
@@ -104,7 +156,7 @@ class customer_InfoWindow(QWidget, Ui_customer_Info):
         account_InfoWindow.show()
 
 class profile_editWindow(QWidget, Ui_profile_edit):
-    profile_username = ""
+    profile_customer_id = ""
 
     def __init__(self):
         super().__init__()
@@ -113,15 +165,22 @@ class profile_editWindow(QWidget, Ui_profile_edit):
 
     def initUi(self):
         self.pushButton.clicked.connect(lambda: self.submit())
+        customer_InfoWindow.initUi()
 
     def submit(self):
         edit_name = self.lineEdit.text()
-        edit_gender = self.comboBox.currentText()
-        edit_birthday = self.dateEdit.date()
+        edit_gender = FrontHelper.StringToGender(self.comboBox.currentText())
+        edit_birthday = self.dateEdit.date().toString("yyyy-MM-dd")
         edit_email = self.lineEdit_4.text()
         edit_msg = self.plainTextEdit.toPlainText()
-        edit_public = self.comboBox_2.currentText()
-        #print(edit_name, edit_gender, edit_birthday, edit_email, edit_msg, edit_public)
+        edit_is_public = FrontHelper.StringToIsPublic(self.comboBox_2.currentText())
+        print(edit_name, edit_gender, edit_birthday, edit_email, edit_msg, edit_is_public)
+        demo.update_profile(self.profile_customer_id, edit_name, edit_gender, edit_birthday, edit_email, edit_msg, None,edit_is_public)
+
+        customer_InfoWindow.close()
+        customer_InfoWindow.initUi()
+        customer_InfoWindow.show()
+        profile_editWindow.close()
 
 
 class account_InfoWindow(QMainWindow, Ui_account_Info):
@@ -174,6 +233,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     loginWindow = loginWindow()
+    demo = functions.WeConnect()
 
     signin_FaceIDWindow = signin_FaceIDWindow()
     signin_PasswordWindow = signin_PasswordWindow()
